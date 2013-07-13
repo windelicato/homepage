23c4759385044c45740fe7c272008780e5004c6a
wei001 <wei001@bucknell.edu>
2013-07-13 10:46:23 -0400

    Content update

---
+++ js/main.js
@@ -0,0 +1,100 @@
+$(document).ready(function() {
+	$('#term').terminal(function(command,term) {
+		if(command == 'test') {
+			term.echo("you typed test");
+		} else if(command == 'about') {
+			term.echo("\
+				<table>\
+				<tr>\
+				<td><img src='img/headshot.jpeg' width=100px></td>\
+				<td>I'm a <span style='color: #d6c3b6'>computer engineer</span> based out of New York City\n\
+				with an electrical engineering background. This \n\
+				is supplemented by an interest in <span style='color: #d6c3b6'>UI design</span> and \n\
+				an extensive <span style='color: #d6c3b6'>music career</span> as a bassist.</td>\
+				</tr>\
+				</table>\
+				", {raw: true});
+		} else if(command == 'contact') {
+			term.echo("\You can email me at<br \>\
+				<span style='color: #d6c3b6'>windelicato</span> AT \
+				<span style='color: #d6c3b6'>gmail.com</span><br \><br \>\
+				Or, chat with me over IRC at<br \>\
+				username: <span style='color: #d6c3b6'>earsplit</span><br \>\
+				network: <span style='color: #d6c3b6'>&#160irc.unixhub.net</span><br \>\
+				channel: <span style='color: #d6c3b6'>&#160#unixhub</span></span>\
+				", {raw: true});
+		} else if(command == 'links') {
+			term.echo("\
+				<table border='0'>\
+				<tr>\
+					<td><a href='http://www.github.com/windelicato'>github</a></td>\
+					<td><span style='color: #9f7155'>««</span> code portfolio</td>\
+				</tr>\
+				<tr>\
+					<td><a href='http://earspl1t.deviantart.com'>deviantart</a></td>\
+					<td><span style='color: #9f7155'>««</span> ui design</td>\
+				</tr>\
+				<tr>\
+					<td><a href='http://www.linkedin.com/in/billindelicato'>linkedin</a></td>\
+					<td><span style='color: #9f7155'>««</span> professionalism</td>\
+				</tr>\
+				<tr>\
+					<td><a href='http://www.reddit.com/u/earsplit'>reddit</a></td>\
+					<td><span style='color: #9f7155'>««</span> karma</td>\
+				</tr>\
+				<tr>\
+					<td><a href='http://www.facebook.com/bill.indelicato'>facebook</a></td>\
+					<td><span style='color: #9f7155'>««</span> social</td>\
+				</tr></table>\
+				", {raw: true});
+		} else if(command == 'help') {
+			term.echo("clear   help   about   contact   links ");
+		} else if(command == 'ls') {
+			term.echo("CNAME  <span style='color: #897796'>css</span>  <span style='color: #897796'>fonts</span>  <span style='color: #897796'>img</span>  index.html  <span style='color: #897796'>js</span>", {raw: true});
+		} else if(command == 'ls -lsa') {
+			term.echo("\
+				total 48<br \>\
+				 4 drwxr-xr-x  7 windelicato users  4096 Jul 12 19:30 .<br \>\
+				 4 drwx------  6 windelicato users  4096 Jul 10 08:28 ..<br \>\
+				 4 -rw-r--r--  1 windelicato users  &#160 16 Jul  10 08:58 CNAME<br \>\
+				 4 drwxr-xr-x  2 windelicato users  4096 Jul 12 09:35 css<br \>\
+				 4 drwxr-xr-x  2 windelicato users  4096 Jul 10 10:18 fonts<br \>\
+				 4 drwxr-xr-x  8 windelicato users  4096 Jul 11 20:40 .git<br \>\
+				 4 drwxr-xr-x  2 windelicato users  4096 Jul 11 20:02 img<br \>\
+				 4 -rw-r--r--  1 windelicato users  2367 Jul 12 19:30 index.html<br \>\
+				 4 drwxr-xr-x  2 windelicato users  4096 Jul 12 19:43 js<br \>\
+			", {raw: true});
+		} else {
+			term.echo( command + " not found. Type <span style='color:#D6C3B6'>help</span> for command list"
+			,{raw: true});
+		}
+			
+	}, {
+		prompt: '» ', 
+		name:'urxvt', 
+		height:'300px',
+		width:'400px',
+		greetings:greeter
+		});
+
+	function greeter() {
+		return "Type help for list of commands ... "
+	}
+
+	function updateClock(){
+		var currentTime = new Date();
+		var currentHours = currentTime.getHours ( );
+		var currentMinutes = currentTime.getMinutes ( );
+		var currentSeconds = currentTime.getSeconds ( );
+		currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+		currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+		var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
+		currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+		currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+		var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+		document.getElementById("clock").firstChild.nodeValue = currentTimeString;
+	} setInterval(updateClock, 100);
+
+	$('.floating').draggable().resizable();
+
+});
